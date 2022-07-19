241244fa4e4e696efa30f0ad0edcb3f79e582b5e
lifeforme <siyaga123@gmail.com>
2022-07-19 15:07:17 +0700

    merapihkan komen

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.get("/addasync", async (req, res) => {
   }
 })
 
-
+// home pages
 app.get('/', (req, res) => {
 
 
@@ -255,10 +255,7 @@ app.post('/contact/update', [
 
 
 });
-// app.post('/contact', (req, res) => {
-//     addDataContact(req.body)
-//     res.send('the data is added!')
-// })
+
 // membuat Detail per contacts
 app.get('/contact/:name', (req, res) => {
   const name = req.params.name;
@@ -275,83 +272,12 @@ app.get('/contact/:name', (req, res) => {
 
 })
 
-
-
-// membuat SaveData Contact atau tambah contact 
-
-
-//Membuat reques
-app.get('/product/:id?', (req, res) => {
-  // res.send('Product Id: ' + req.params.id + '<br>'
-  // + 'Category Id : ' + req.params.idCat);
-  let category = req.query.category;
-  res.send(`Product Id : ${req.params.id} <br> Category Id : ${category}`);
-})
-
+// jika tidak ada req get error
 app.use('/', (req, res) => {
   res.status(404)
   res.send('Page Not found : 404')
 })
-
+// cek port
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
-
-
-
-
-// const http = require('http');   
-// const port = 3000;
-// const fs = require('fs'); 
-
-// const findRespown = (url, res)=>{
-
-//     fs.readFile(url,(err,data)=> {
-
-//         if(err){
-//             res.writeHead(404);
-//             res.write('Error : page not found');
-//         } else {
-//             res.write(data);
-//         }
-//         res.end();
-//     })
-
-// }
-
-// http
-//     .createServer((req,res)=>{
-
-//         //membuat fungsi validasi
-//         const url = req.url;
-//         console.log(url);
-//         // Menambahkan validasi untuk setiap pagenya
-//         if(url==='/about'){
-//             // res.write('<h1>this is about page</h1>');
-//             // res.end();
-//             findRespown('./view/about.html',res);
-//             // fs.readFile('./view/about.html',(err,data)=> {
-//             //     validasiData(err,data);
-//             // })
-//         }else  if(url==='/contact'){
-//             // res.write('<h1>this is contact page</h1>');
-//             // res.end();
-//             findRespown('./view/contact.html',res);
-//             // fs.readFile('./view/contact.html',(err,data)=> {
-//             //     validasiData(err,data);
-//             // })
-
-//         }else {
-//             findRespown('./view/index.html',res);
-//         // res.write('hello world');
-//         // res.end();
-//         }
-//         // res.writeHead(200, { 
-//         //     'Content-Type': 'text/html' });
-
-//     })
-//     // Memasukan Port yang akan di jalankan
-//     .listen(port, ()=>{
-//         console.log('Server listening on port 3000');
-//     });
